8cc073b8d9fd49842a01a03a2c687579422ca612
Olim <olim04@web.de>
2025-03-06 15:03:27 +0100

    added acronyms, cleaned 30_, added example script

--- resources/example-script.py
+++ resources/example-script.py
@@ -1,4 +1,23 @@
-print("Hello World")
+# Sample airline document
+sample_airline = {
+	"type": "airline",
+	"id": 8091,
+	"callsign": "CBS",
+	"iata": None,
+	"icao": None,
+	"name": "Couchbase Airways",
+}
+key = "airline_8091"
 
-for i in range(0,10):
-    print("Example " + str(i))
+# Create a document with specific ID
+result = collection.insert(key, sample_airline)
+
+# Retrieve a document by ID
+result = collection.get(key)
+
+# Update a document by ID
+sample_airline["name"] = "Couchbase Airways!!"
+result = collection.replace(key, sample_airline)
+
+# Delete a document by ID
+result = collection.remove(key)
